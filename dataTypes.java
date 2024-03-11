32308724f02e09e3963b63418b56e3925818366d
jangamn <158302534+jangamn@users.noreply.github.com>
2024-03-11 20:45:47 +0530

    Add files via upload

---
+++ dataTypes.java
@@ -0,0 +1,56 @@
+package JavaConcepts;
+
+public class dataTypes {
+    public static void main(String[] args){
+        //Primitive & Non-Primitive Data Types
+
+        //Non-Primitive Data Types: Class, Arrays, Interface & Strings.
+
+        //Primitive Data Types: Integers, Decimals, Boolean & Characters.
+
+        //Integers:
+        /*
+        byte - Size is 2 power 8- Range is -128 to -1 & 0 to 127.
+        short - Size is 2 power 16 - Range is -32768 to -1 & 0 to 32767.
+        int - Size is 2 power 32.
+        long - Size is 2 power 64.
+         */
+
+        //Decimals:
+        /*
+        float - Size is 2 power 32.
+        double - Size is 2 power 64.
+         */
+
+        //Characters
+        /*
+        char - Single quote & Single Letter.
+        Size is 2 power 32.
+         */
+
+        //Boolean:
+        //boolean - Has only 2 values - true/false
+
+        byte bb; //Declaration
+        byte a = 4; //Initialization
+        short b = 129;
+        int c = 300;
+        long d = -1234514;
+
+        float e = 1.2f;
+        double g = 3.1234;
+
+        boolean h = true;
+        boolean i = false;
+
+        char j = 'a';
+        char k = '1';
+        char l = 97;
+
+        System.out.println(a+5);
+        System.out.println("100");
+        System.out.println(a+b);
+        System.out.println(a+'a');
+        System.out.println("a"+'a'+'b');
+    }
+}
